16997eceeae2297799a687e5e783aed8db22a3c7
Manuel <manuelcolmenaresunerg@gmail.com>
2022-11-18 01:07:13 -0300

    Creacion del PUT y realice las modificaciones sugeridas por el tutor

--- routes/productos.js
+++ routes/productos.js
@@ -1,11 +1,10 @@
-const fs = require('fs')
 const { Router } = require('express');
 const router = Router();
 
 
-const products = JSON.parse(fs.readFileSync('./productos.txt', 'utf-8')); // Obtenemos el array de productos.
-//console.log(productos);
-products.length + 1
+const products = []
+
+   
 
 router.get('/', (req, res) => { // Devuelve todos los productos.
     res.json(products)
@@ -18,16 +17,40 @@ router.get('/', (req, res) => { // Devuelve todos los productos.
     else res.json(product);
  });
 
- router.post('/', (req, res) => {
+ router.post('/', (req, res) => {  // Recibe y agrega un producto, y lo devulve con su id asignado
+
+    const getId = (arr) => { 
+        if (arr.length == 0) return 1;
+        else return arr[arr.length -1].id + 1;
+    }
+    const id = getId(products);
+
     const product = {
         title: req.body.title,
         precio: parseInt(req.body.precio),
         thumbnail: req.body.thumbnail,
-        id: products.length + 1
+        id: id
+
     };
     products.push(product);
     res.json(products);
 });
+
+router.put('/:id', (req, res,) => {  // Recibe y actualiza un producto según su id. 
+    
+    const product = products.find(c => c.id === parseInt(req.params.id));
+    const { title, precio, thumbnail} = req.body;
+
+    //const error = {error: 'producto no encontrado'};
+    if (!product) return res.json('Por favor ingrese un id valido');
+    
+    product.title = title
+    product.precio = precio
+    product.thumbnail = thumbnail
+
+    res.json(product);
+
+ });
  
 router.delete('/:id', (req, res) => {  // Elimina un producto según su id. 
     const product = products.find(c => c.id === parseInt(req.params.id));
